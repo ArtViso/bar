fb0f6f2d30b8f05347fd9b9cc84d4cd517140075
ArtViso <off.club1515@gmail.com>
2021-04-21 12:06:48 +0300

    KRFIN

--- js/script.js
+++ js/script.js
@@ -1,8 +1,7 @@
-function myMap() {
-    var mapCanvas = document.getElementById("map");
-    var mapOptions = {
-        center: new google.maps.LatLng(51.5, -0.2),
-        zoom: 10
-    };
-    var map = new google.maps.Map(mapCanvas, mapOptions);
-}
+$('.autoplay').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  arrows: false,
+});
